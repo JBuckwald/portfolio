a0c92b4f7724ab4db02b49c1f44d5c29d972e347
Josh Buckwald <josh.buckwald@gmail.com>
2025-07-28 15:56:06 -0700

    initial setup

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  eslintIntegration: true,
+};
