e5d077dbb0d45414d4ca49267ccee9da7c586869
Josh Buckwald <josh.buckwald@gmail.com>
2025-07-28 23:13:41 -0700

    changed background color

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,12 +1,15 @@
+import headshot from '../assets/images/headshot.png';
+
 export function IntroSection() {
   return (
     <div>
+      <img src={headshot} className="rounded-full" />
       <div>
         <div>skills</div>
         <div>intro</div>
       </div>
-
-      <div>hero image</div>
     </div>
   );
 }
+
+// All layout should be flex boxes and grid boxes.
--- src/components/ProjectSection/Project.tsx
+++ src/components/ProjectSection/Project.tsx
@@ -7,3 +7,6 @@ export function Project() {
     </div>
   );
 }
+
+// Look up the React Concept of PROPS ... that's how we dynamically use this file as a template.
+// This project takes an image url and text.... blah blah
